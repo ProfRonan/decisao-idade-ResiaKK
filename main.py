49651ec5d8e9de117c3477a92a02c0f732e7dbb0
Kelvin Kaiser Sousa Carneiro <kelvin.ksc10@gmail.com>
2023-04-23 14:06:21 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+idade = int(input("Digite a sua idade:  "))
+if idade<0:
+    print("impossível!")
+
+if idade<18:
+        print("não precisa se alistar.")
+
+if 65>idade>18:
+    print("Não esqueça de votar na próxima eleição.")
+
+if idade>65:
+     print("Vá descansar.")  
+
+if idade==18:
+     print("eita!")       
+
+if idade==65:
+     print("eita!")           
+
+    
+            
+        
+
+
+        
+    
+
+
+
+
+
+          
+    
+          
+
+          
+        
+  
